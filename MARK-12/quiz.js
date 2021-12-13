ce57fa32103dbf95f7459ff3db7e4ee013c44cad
sonamng <86510537+sonamng@users.noreply.github.com>
2021-12-13 16:17:12 +0530

    Add files via upload

---
+++ MARK-12/quiz.js
@@ -0,0 +1,56 @@
+
+// const quizForm=document.querySelector("#question-container")
+// const submitAnswer=document.querySelector("#submit-answer-btn")
+// const outputEl=document.querySelector("#output")
+
+// const correctAnswers=["90°","right angled"];
+
+// function calculateScore(){
+//     let score=0;
+//     let index=0;
+
+//     const formResults=new FormData(quizForm);
+//     for(let value of formResults.values()){
+//         if(value===correctAnswers[index]){
+//             score=score+1
+//         }
+//         index=index+1
+//     }
+//     console.log(score)
+//     outputEl.inner.Text="your score is"+score;
+// }
+
+
+
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-answers-btn");
+const outputEl = document.querySelector(".output");
+
+const correctAnswers = [
+  "90°",
+  "right angled",
+  "one right angle",
+  "12, 16, 20",
+  "Equilateral triangle",
+  "100°",
+  "30°",
+  "a + b + c",
+  "no",
+  "45°",
+];
+
+function calculateScore() {
+  const formResults = new FormData(quizform);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  // console.log("The score is "+score);
+  outputEl.innerText = "The score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
